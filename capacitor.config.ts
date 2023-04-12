946c77e65ab26a96cb6968c39e5afc7076fb1563
goutham375 <goutham375@gmail.com>
2023-04-12 12:05:09 +0530

    angular to anroid changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: ' mymessages',
+  webDir: 'dist/mymessages',
+  bundledWebRuntime: false
+};
+
+export default config;
